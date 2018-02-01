78b9f930773ff91257460e01636775cb6b21de3a
周韬健 <zhoutaojian@wxchina.com>
2018-02-01 18:57:57 +0800

    init

---
+++ feimer-core/src/main/java/ee/java2/feimer/FeimerCoreApplication.java
@@ -0,0 +1,12 @@
+package ee.java2.feimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeimerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeimerCoreApplication.class, args);
+	}
+}
